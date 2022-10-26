63e194c2803124dc133a04c699d5cc7c10cbeea7
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-10-26 15:37:57 +0200

    adding bean validation. adding test for beerController

--- src/main/java/com/dzmitry/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/dzmitry/msscbrewery/web/controller/BeerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 //@Deprecated
@@ -25,7 +26,7 @@ public class BeerController {
 
     //POST - create new beer
     @PostMapping
-    public ResponseEntity<BeerDTO> handlePost(@RequestBody BeerDTO beerDTO) {
+    public ResponseEntity<BeerDTO> handlePost(@Valid @RequestBody BeerDTO beerDTO) {
         BeerDTO savedDto = beerService.saveNewBeer(beerDTO);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer" + savedDto.getId().toString());
@@ -33,7 +34,7 @@ public class BeerController {
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity<BeerDTO> handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDTO beerDTO) {
+    public ResponseEntity<BeerDTO> handleUpdate(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDTO beerDTO) {
 
         beerService.updateBeer(beerId, beerDTO);
 
--- src/main/java/com/dzmitry/msscbrewery/web/model/BeerDTO.java
+++ src/main/java/com/dzmitry/msscbrewery/web/model/BeerDTO.java
@@ -2,6 +2,9 @@ package com.dzmitry.msscbrewery.web.model;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.util.UUID;
 
 @Data
@@ -10,8 +13,15 @@ import java.util.UUID;
 @Builder
 public class BeerDTO {
 
+    @Null
     private UUID id;
+
+    @NotBlank
     private String beerName;
+
+    @NotBlank
     private String beerStyle;
+
+    @Positive
     private Long upc;
 }
